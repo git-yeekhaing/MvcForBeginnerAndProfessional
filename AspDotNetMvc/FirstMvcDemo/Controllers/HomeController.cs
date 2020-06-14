5510122f3b08ad71d64692f7729302103902e8ca
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-14 18:43:47 +0900

    add the view Index.cshtml

--- AspDotNetMvc/FirstMvcDemo/Controllers/HomeController.cs
+++ AspDotNetMvc/FirstMvcDemo/Controllers/HomeController.cs
@@ -8,11 +8,10 @@ namespace FirstMvcDemo.Controllers
 {
     public class HomeController : Controller
     {
-        //http://localhost:xxxx/Home/Index/10?name=James
         // GET: Home
-        public string Index(int id, string name)
+        public ActionResult Index()
         {
-            return "the value of Id = " + id + " and Name = " + Request.QueryString["name"];
+            return View();
         }
     }
 }
