320d061758db44688316b88c88c36891eed54cc1
git-yeekhaing <yee.khaing.git@gmail.com>
2020-06-22 18:56:01 +0900

    added strongly typed

---
+++ AspDotNetMvc/myExercises/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using myExercises.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myExercises.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+            Employee employee = employeeBL.GetEmployeeDetails(102);
+
+            ViewData["Header"] = "Employee Details";
+
+            return View(employee);
+        }
+    }
+}
---
+++ AspDotNetMvc/myExercises/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myExercises.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public Employee GetEmployeeDetails(int EmployeeId)
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = EmployeeId,
+                Name = "Pranaya",
+                Gender = "Male",
+                City = "Mumbai",
+                Salary = 1000,
+                Address = "Andheri"
+            };
+
+            return employee;
+        }
+    }
+}
